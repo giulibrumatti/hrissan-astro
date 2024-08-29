7b7e375e206bfbba5bb4b20574d08a34b060f9f7
giulibrumatti <giulibrumatti97@gmail.com>
2024-08-29 08:51:18 -0300

    add project

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#b83a43",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+			},
+			screens:{
+				midmd:"880px"
+			},
+			fontFamily:{
+				tourney:['Tourney Variable']
+			},
+			fontWeight: {
+			  normal: 500,
+			  bold: 900,
+			},
+		},
+	},
+	plugins: [],
+}
