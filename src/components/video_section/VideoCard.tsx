3add13228e9b4b55e0b8982d48ec00c5f6178ef1
giulibrumatti <giulibrumatti97@gmail.coim>
2024-11-28 14:16:34 -0300

    personalizando seccion video

--- src/components/video_section/VideoCard.tsx
+++ src/components/video_section/VideoCard.tsx
@@ -8,7 +8,7 @@ export default function VideoCard(props: VideoCardProps) {
   const { videos } = props;
 
   return (
-    <div className="flex flex-col bg-primary dark:bg-dk-primary rounded-lg">
+    <div className="flex flex-col bg-primary rounded-2xl">
       <div className="flex-shrink-0">
         <a
           href={
@@ -17,7 +17,7 @@ export default function VideoCard(props: VideoCardProps) {
               : `https://www.youtube.com/watch?v=${videos.videoId}`
           }
           rel="noreferrer"
-          aria-label={videos.img_alt + ", click to open the project page"}
+          aria-label={videos.img_alt} className="rounded-2xl"
         >
           <img
             alt={videos.title}
@@ -26,13 +26,13 @@ export default function VideoCard(props: VideoCardProps) {
                 ? `${videos.img_path}`
                 : `https://i.ytimg.com/vi/${videos.videoId}/maxresdefault.jpg`
             }
-            className="h-55 w-full object-cover"
+            className="h-55 w-full object-cover rounded-2xl"
           />
         </a>
       </div>
-      <div className="flex-1 bg-primary dark:bg-dk-primary p-6 flex flex-col justify-between">
+      <div className="flex-1 bg-primary p-6 flex flex-col justify-between rounded-2xl">
         <div className="flex-1">
-          <p className="text-sm font-medium text-text dark:text-dk-text">
+          <p className="text-sm font-medium text-slate-400">
             <a
               href={
                 videos.link
@@ -40,8 +40,8 @@ export default function VideoCard(props: VideoCardProps) {
                   : `https://www.youtube.com/watch?v=${videos.videoId}`
               }
               rel="noreferrer"
-              className="hover:underline"
-              aria-label="Title of the project, click to open the project page"
+              className="hover:underline rounded-2xl"
+              aria-label=""
             >
               {videos.title}
             </a>
@@ -53,21 +53,21 @@ export default function VideoCard(props: VideoCardProps) {
                 : `https://www.youtube.com/watch?v=${videos.videoId}`
             }
             rel="noreferrer"
-            className="block mt-2"
-            aria-label="Description of the project, click to open the project page"
+            className="block rounded-2xl"
+            aria-label=""
           >
-            <p className="text-xl font-semibold text-gray-900">
+            <p className="text-lg font-bold text-white">
               {videos.description}
             </p>
           </a>
         </div>
-        <div className="mt-6 flex items-center">
-          <div className="flex flex-wrap space-x-2">
-            <span className="text-dk-accent text-xs font-semibold">
+        <div className="mt-2 flex items-center rounded-2xl">
+          <div className="flex flex-wrap space-x-2 rounded-2xl">
+            <span className="text-xs font-semibold text-slate-400 rounded-2xl">
               {videos.tech}
             </span>
           </div>
-          <p className="ml-auto text-sm font-medium text-gray-900">
+          <p className="ml-auto text-sm font-medium text-slate-400">
             {videos.date}
           </p>
         </div>
